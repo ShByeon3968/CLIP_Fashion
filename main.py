1ffb97c27ec071d58604b3720af769aa90305d7c
SangHyun Byun <101382267+ShByeon3968@users.noreply.github.com>
2025-05-19 00:00:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from text_parser.parse_text import TextParser
-from animation_diff.generate_frames import AnimateDiff
+from animation_diff.generate_frames import LTXVideoDiff
 
 if __name__ == "__main__":
     # Initialize the TextParser
     text_parser = TextParser()
     # Initialize the AnimateDiff
-    animate_diff = AnimateDiff()
+    animate_diff = LTXVideoDiff()
 
     # Example input
     text = "A man in a suit enters a room and speaks to two women sitting on a couch. The man, wearing a dark suit with a gold tie, enters the room from the left and walks towards the center of the frame. He has short gray hair, light skin, and a serious expression. He places his right hand on the back of a chair as he approaches the couch. Two women are seated on a light-colored couch in the background. The woman on the left wears a light blue sweater and has short blonde hair. The woman on the right wears a white sweater and has short blonde hair. The camera remains stationary, focusing on the man as he enters the room. The room is brightly lit, with warm tones reflecting off the walls and furniture. The scene appears to be from a film or television show."
